dd80c329d7c5fe88ec61560533eb91beced3058d
ThibautDeduit <149255011+ThibautDeduit@users.noreply.github.com>
2024-02-05 17:41:42 +0100

    Let buttons send values to display

--- script.js
+++ script.js
@@ -1,35 +1,72 @@
-function add(x, y) {
-	return x + y
+let displayVal = 0
+let operator = ""
+
+function startCalculator() {
+	let buttons = document.querySelectorAll("button")
+	for (let button of buttons) {
+		switch (button.className) {
+			case "digit":
+				button.addEventListener("click", pressDigit)
+				break
+			case "decimal":
+				button.addEventListener("click", pressDecimal)
+				break
+			case "add":
+				button.addEventListener("click", pressAdd)
+				break
+			case "subtract":
+				button.addEventListener("click", pressSubtract)
+				break
+			case "multiply":
+				button.addEventListener("click", pressMultiply)
+				break
+			case "divide":
+				button.addEventListener("click", pressDivide)
+				break
+			case "equal":
+				button.addEventListener("click", pressEqual)
+				break
+		}
+	}	
 }
 
-function subtract(x, y) {
-	return x - y
+
+function pressDigit(e) {
+	let digit = e.target.textContent
+	displayVal += digit
+	updateDisplay()
 }
 
-function multiply(x, y) {
-	return x * y
+function pressAdd() {
+	displayVal += "+"
+	updateDisplay()
 }
 
-function divide(x, y) {
-	return x / y
+function pressEqual() {
+	displayVal = "Error: noMath"
+	updateDisplay()
 }
 
-function operate(x, y, operator) {
-	switch (operator) {
-		case "+":
-			return add(x, y)
-			break
-		case "-":
-			return subtract(x, y)
-			break
-		case "*":
-			return multiply(x, y)
-			break
-		case "/":
-			return divide(x, y)
-			break
-		default:
-			return "Error: wrong operator"
-			break
-	}
+function pressDecimal() {
+	displayVal += "."
+	updateDisplay()
 }
+function pressSubtract() {
+	displayVal += "-"
+	updateDisplay()
+}
+function pressMultiply() {
+	displayVal += "*"
+	updateDisplay()
+}
+function pressDivide() {
+	displayVal += "/"
+	updateDisplay()
+}
+
+function updateDisplay() {
+	let display = document.querySelector(".display")
+	display.textContent = displayVal
+}
+
+startCalculator()
